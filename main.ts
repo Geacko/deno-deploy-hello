c85687b7ff76bb8da642a5e645bd3d56143c557a
Geacko <gregoirejacquot@outlook.com>
2023-01-07 12:10:42 +0100

    readFile

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import {
     serve
 } from 'https://deno.land/std@0.171.0/http/server.ts'
 
-const content = Deno.readFileSync(new URL('index.html', import.meta.url))
+const content = await Deno.readFile(new URL('index.html', import.meta.url))
 
 // execute ...
 serve(() => {
