92eeb2e22be3643e993495eedae8317df30036a7
Geacko <gregoirejacquot@outlook.com>
2023-01-07 12:21:57 +0100

    add sanity

--- main.ts
+++ main.ts
@@ -2,21 +2,23 @@ import {
     serve
 } from 'https://deno.land/std@0.171.0/http/server.ts'
 
-/** @internal */
-const uri 
-    = new URL( 'index.html' , import.meta.url )
-
 // execute ...
 serve(async () => {
-    
-    const content 
-        = await Deno.open(uri)
 
-    /** @todo */
-    // ...
-    // ...
-    // ...
+    const response = await fetch('https://u5kchvov.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%3D%3D%22tip%22%5D%20%7B%0A%20%20title%0A%7D')
     
-    return new Response( content.readable )
+    let content = ''
+
+    if (response.ok) {
+
+        const xs = await response.json() as {title : string}[]
+        
+        for (const x of xs) {
+            content += x.title + '\n'
+        }
+
+    }
+
+    return new Response(content)
 
 })
