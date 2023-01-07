408dfa4fd49d89492394eeb3e6c611fad312531e
Geacko <gregoirejacquot@outlook.com>
2023-01-07 11:40:05 +0100

    update server #2

--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-import {
-    serve
-} from 'https://deno.land/std@0.171.0/http/server.ts'
-
-serve(() => {
-    return new Response('Welcome...')
+Deno.serve(() => {
+    return new Response('Welcome...')  
 })
