5abee1344f0229c3e9ab30a31da38a38a038d282
Geacko <gregoirejacquot@outlook.com>
2023-01-07 11:41:16 +0100

    back to std

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-Deno.serve(() => {
-    return new Response('Welcome...')  
+import {
+    serve
+} from 'https://deno.land/std@0.171.0/http/server.ts'
+
+// execute ...
+serve(() => {
+    
+    /** @todo */
+    // ...
+    // ...
+    // ...
+    
+    return new Response('Welcome...')
+
 })
